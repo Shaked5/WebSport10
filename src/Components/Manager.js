5b7a47355f5400f12790701a98cc6e5dfbcfb18c
Shaked5 <shakedshervi@gmail.com>
2021-01-20 10:04:05 +0200

    add local storage for add team to array

--- src/Components/Manager.js
+++ src/Components/Manager.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';  
+import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import Home from './Home';
 import maccabiIcon from '../images/maccabitlv.png';
@@ -10,60 +10,72 @@ class Manager extends Component {
     constructor(props) {
         super(props);
         this.counter = 5;
-        this.state = { 
-            teams:[
-                { id: 1,
-                 club:"מכבי תל אביב" ,
-                 items: [] ,
-                 imgClub:"https://upload.wikimedia.org/wikipedia/he/thumb/8/82/Maccabi_tlv_fc.png/250px-Maccabi_tlv_fc.png",
-                 players:[{id: 1 , name: "jkjk" , age:0 }] 
+        this.state = {
+            teams: [
+                {
+                    id: 1,
+                    club: "מכבי תל אביב",
+                    items: [],
+
+                    imgClub: "https://upload.wikimedia.org/wikipedia/he/thumb/8/82/Maccabi_tlv_fc.png/250px-Maccabi_tlv_fc.png",
+                    players: [{ id: 1, name: "jkjk", age: 0 }]
                 },
-                { id:2,
-                club:"מכבי חיפה",
-                items:[],
-                imgClub:"https://newshaifakrayot.net/wp-content/uploads/2019/08/214px-Maccabi-h.png",
-                players:[{id:1, name: "dsaafd" , age:28}]
+                {
+                    id: 2,
+                    club: "מכבי חיפה",
+                    items: [],
+                    imgClub: "https://newshaifakrayot.net/wp-content/uploads/2019/08/214px-Maccabi-h.png",
+                    players: [{ id: 1, name: "dsaafd", age: 28 }]
                 },
-                { id:3,
-                club:"מכבי פתח תקווה",
-                items:[],
-                imgClub:"https://m-pt.co.il/wp-content/uploads/2020/10/maccabi_petah-tikva.png",
-                players:[{id:1, name: "vcxvx",age:22}]
+                {
+                    id: 3,
+                    club: "מכבי פתח תקווה",
+                    items: [],
+                    imgClub: "https://m-pt.co.il/wp-content/uploads/2020/10/maccabi_petah-tikva.png",
+                    players: [{ id: 1, name: "vcxvx", age: 22 }]
                 },
-                { id:4,
-                club:"ביתר ירושלים",
-                items:[],
-                imgClub:"https://sheva7.co.il/wp-content/uploads/2018/10/%D7%9C%D7%95%D7%92%D7%95-%D7%91%D7%99%D7%AA%D7%A8-%D7%99%D7%A8%D7%95%D7%A9%D7%9C%D7%99%D7%9D.png",
-                players:[{id:1 , name: "sdada", age:19}]
+                {
+                    id: 4,
+                    club: "ביתר ירושלים",
+                    items: [],
+                    imgClub: "https://sheva7.co.il/wp-content/uploads/2018/10/%D7%9C%D7%95%D7%92%D7%95-%D7%91%D7%99%D7%AA%D7%A8-%D7%99%D7%A8%D7%95%D7%A9%D7%9C%D7%99%D7%9D.png",
+                    players: [{ id: 1, name: "sdada", age: 19 }]
                 },
-                {id:5,
-                club:"הפועל באר שבע",
-                items:[],
-                imgClub:"https://lh3.googleusercontent.com/proxy/v127R82MjX5XLmoAqx_sy2RBknjIXgw_b9FFhV_9C6_VBB-IAnUdZ0o7mmq-QQCdidKYWTju4i9eib7IiuBxWsY",
-                players:[{id:1 , name: "weqeq",age:23}]
+                {
+                    id: 5,
+                    club: "הפועל באר שבע",
+                    items: [],
+                    imgClub: "https://lh3.googleusercontent.com/proxy/v127R82MjX5XLmoAqx_sy2RBknjIXgw_b9FFhV_9C6_VBB-IAnUdZ0o7mmq-QQCdidKYWTju4i9eib7IiuBxWsY",
+                    players: [{ id: 1, name: "weqeq", age: 23 }]
                 }
-                ]
-         }
+            ]
+        }
     }
 
-    getTeamFromChild=(data)=>{
-    let newTeam={id:++this.counter,club:data.club,items:[],imgClub:data.imgClub,players:data.players}
-    this.setState({teams:[...this.state.teams,newTeam]})
+    getTeamFromChild = (data) => {
+        debugger;
+        let newTeam = { id: ++this.counter, club: data.club, items: [], imgClub: data.imgClub, players: data.players }
+        this.setState({
+            teams: [...this.state.teams, newTeam]
+
+        }, () => {
+            localStorage.setItem('teams',JSON.stringify(this.state.teams))
+        })
     }
- 
-    render() { 
-         console.log(this.state.teams);
-        return ( 
+
+    render() {
+        console.log(this.state.teams);
+        return (
             <Switch>
                 <Route exact path="/" component={Home}></Route>
-                <Route path="/teams" render={()=> <Teams teams={this.state.teams} sendToParent={this.getTeamFromChild}/>}></Route>
+                <Route path="/teams" render={() => <Teams teams={this.state.teams} sendToParent={this.getTeamFromChild} />}></Route>
                 <Route path="/table_rank"></Route>
                 <Route path="/store"></Route>
                 <Route path="/cart"></Route>
                 <Route path="/gamble"></Route>
             </Switch>
-         );
+        );
     }
 }
- 
+
 export default withRouter(Manager);
