bd1e0c1b0701ff4bfe21e92589535db1dc3b1a1a
Netanel Aknin <netanelos152@gmail.com>
2021-02-14 14:25:31 +0200

    add photo to table comp

--- src/Components/TableTeams.js
+++ src/Components/TableTeams.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import NavWeb from './Nav';
 import '../CSSWeb/table.css';
 import { withRouter } from 'react-router-dom';
-
+import israelLig from '../images/israelLeage.png';
 class TableTeams extends Component {
   constructor(props) {
     super(props);
@@ -68,7 +68,7 @@ class TableTeams extends Component {
         <NavWeb />
         <div>
           <center>
-            <h3 style={{marginTop: 10, textAlign: 'center', color: 'lightseagreen', fontStyle: 'oblique', fontSize: 46 }}>דירוג הקבוצות</h3>
+            <h3 style={{ marginTop: 10, textAlign: 'center', color: 'lightseagreen', fontStyle: 'oblique', fontSize: 46 }}>דירוג הקבוצות</h3>
             <table id="containerTable">
               <tbody>
                 <tr style={{ fontSize: '20px', fontWeight: 'bold', color: 'blue', backgroundColor: 'lightblue' }}>
@@ -82,6 +82,9 @@ class TableTeams extends Component {
                 {this.renderTableData()}
               </tbody>
             </table>
+            <div style={{ marginTop: 60}}>
+              <img src={israelLig} style={{width: '50%', borderRadius: 8 , boxShadow: '0 6px 10px 0 rgba(0, 0, 0, 0.2), 0 8px 22px 0'}} />
+            </div>
           </center>
         </div>
       </div>
